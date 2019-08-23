ae70cb464546bbaa83efe31f57b2a35bba957d00
James Miller <james.rex.miller.4@gmail.com>
2019-08-23 11:43:50 -0600

    Add disable functionality to submit button

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var button = document.getElementById('submitbtn');
 var grabSpanQuestion = document.getElementById('insertquestion');
 var grabSpanResult = document.getElementById('insertresult');
 
+var eightball = document.querySelector('.eightballimg');
+var grabH2Question = document.getElementById('displayquest');
+var grabH2Result = document.getElementById('displayresult');
+
+var clearbutton = document.getElementById('clearbtn');
+
 var eightballArray = [
 "It is certain.",
 "It is decidedly so.",
-"Without a doubt."
+"Without a doubt.",
 "Yes - definitely.",
 "You may rely on it.",
 "As I see it, yes.",
@@ -24,11 +30,51 @@ var eightballArray = [
 "My sources say no.",
 "Outlook not so good.",
 "Very doubtful."
-]
+];
 
+function disable() {
+  if (inputquestion.value.length === 0) {
+    return;
+  }
+}
+
+function displayQuestion() {
+    var question = inputquestion.value;
+    grabSpanQuestion.innerHTML = question;
+    event.preventDefault();
+    inputquestion.value = '';
+};
+
+function selectRandomResult() {
+    i = Math.floor(Math.random() * 20)
+    grabSpanResult.innerHTML = eightballArray[i];
+    event.preventDefault();
+};
+
+
+function hocusPocus(){
+      eightball.classList.add('disappear');
+      grabH2Question.classList.remove('disappear');
+      grabH2Result.classList.remove('disappear');
+};
+
+function abraKadabra() {
+      eightball.classList.remove('disappear');
+      grabH2Question.classList.add('disappear');
+      grabH2Result.classList.add('disappear');
+}
 
 button.addEventListener('click', function() {
-    question = inputequestion.value;
-    grabSpanQuestion.innerHTML = question;
+    if (inputquestion.value.length === 0) {
+      return;
+    }
+    displayQuestion();
+    selectRandomResult();
+    hocusPocus();
 
-})
+});
+
+clearbutton.addEventListener('click', function() {
+    document.getElementById('magicform').reset();
+    abraKadabra();
+});
