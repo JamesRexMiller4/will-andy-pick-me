850c49f894cb4e5c82aa9237048884b8c92f035b
James Miller <james.rex.miller.4@gmail.com>
2019-08-23 10:08:13 -0600

    Add results array

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 var inputquestion = document.getElementById('question');
-var
+var button = document.getElementById('submitbtn');
+var grabSpanQuestion = document.getElementById('insertquestion');
+var grabSpanResult = document.getElementById('insertresult');
+
+var eightballArray = [
+"It is certain.",
+"It is decidedly so.",
+"Without a doubt."
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."
+]
+
+
+button.addEventListener('click', function() {
+    question = inputequestion.value;
+    grabSpanQuestion.innerHTML = question;
+
+})
